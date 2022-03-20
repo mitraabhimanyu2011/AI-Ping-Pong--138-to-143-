ecef1cf08e6de2564710408faec4038c4f75b172
mitraabhimanyu2011 <82744734+mitraabhimanyu2011@users.noreply.github.com>
2022-03-19 18:17:23 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,8 +24,19 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+
+  video = createCapture(VIDEO);
+  video.size(600,300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  // poseNet.on("pose", gotPoses);
 }
 
+function modelLoaded()
+{
+  console.log("Model Loaded!");
+}
 
 function draw(){
 
